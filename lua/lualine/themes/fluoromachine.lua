6a16944a65a083f6a3b051ebe2fd3511663b4956
Max <milianordelcanto@gmail.com>
2023-05-19 02:38:22 -0300

    fix lualine fluoromachine invalid colors

--- lua/lualine/themes/fluoromachine.lua
+++ lua/lualine/themes/fluoromachine.lua
@@ -9,24 +9,24 @@ end
 
 return {
   normal = {
-    a = { bg = colors.blended_purple, fg = colors.purple, gui = 'bold' },
+    a = { bg = colors.bg, fg = colors.purple, gui = 'bold' },
     b = { bg = d(colors.purple, 50), fg = colors.purple },
     c = { bg = colors.alt_bg, fg = colors.fg },
   },
   insert = {
-    a = { bg = colors.blended_yellow, fg = colors.yellow, gui = 'bold' },
+    a = { bg = colors.bg, fg = colors.yellow, gui = 'bold' },
     b = { bg = d(colors.yellow, 50), fg = colors.yellow },
   },
   visual = {
-    a = { bg = colors.blended_cyan, fg = colors.cyan, gui = 'bold' },
+    a = { bg = colors.bg, fg = colors.cyan, gui = 'bold' },
     b = { bg = d(colors.cyan, 50), fg = colors.cyan },
   },
   replace = {
-    a = { bg = colors.blended_red, fg = colors.red, gui = 'bold' },
+    a = { bg = colors.bg, fg = colors.red, gui = 'bold' },
     b = { bg = d(colors.red, 50), fg = colors.red },
   },
   command = {
-    a = { bg = colors.blended_orange, fg = colors.orange, gui = 'bold' },
+    a = { bg = colors.bg, fg = colors.orange, gui = 'bold' },
     b = { bg = d(colors.orange, 50), fg = colors.orange },
   },
 }
