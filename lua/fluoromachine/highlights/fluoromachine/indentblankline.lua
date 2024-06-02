88d1f5a0d2b5182933e273354da4eaa5cdaaffef
Max Miliano <milianordelcanto@gmail.com>
2024-06-02 11:46:13 -0300

    fix: indentblankline

--- lua/fluoromachine/highlights/fluoromachine/indentblankline.lua
+++ lua/fluoromachine/highlights/fluoromachine/indentblankline.lua
@@ -1,14 +1,14 @@
 ---@type fm.highlights
 local M = {}
 
-load = function(opts)
+M.load = function(opts)
   local darken = opts.color.darken
   local hl = opts.utils.set_hl
   local c = opts.colors
 
-  hl('IndentBlanklineChar', { fg = darken(c.purple, 20) })                       -- Highlight of indent character. Default: Whitespace
-  hl('IndentBlanklineSpaceChar', { link = 'IndentBlanklineChar' })               -- Highlight of space character. Default: Whitespace
-  hl('IndentBlanklineContextChar', { fg = darken(c.pink, 10) })                  -- Highlight of indent character when base of current context. Default: Label
+  hl('IndentBlanklineChar', { fg = darken(c.purple, 20) }) -- Highlight of indent character. Default: Whitespace
+  hl('IndentBlanklineSpaceChar', { link = 'IndentBlanklineChar' }) -- Highlight of space character. Default: Whitespace
+  hl('IndentBlanklineContextChar', { fg = darken(c.pink, 10) }) -- Highlight of indent character when base of current context. Default: Label
   hl('IndentBlanklineContextSpaceChar', { link = 'IndentBlanklineContextChar' }) -- Highlight of space characters one indent level of the current context. Default: Label
   hl('IblIndent', { fg = darken(c.purple, 20), nocombine = true })
   hl('IblScope', { fg = darken(c.pink, 10), nocombine = true })
