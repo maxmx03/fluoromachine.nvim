f09292a12fe832259489c47efae8ecb566143b8a
Max Miliano <milianordelcanto@gmail.com>
2024-06-11 19:51:46 -0300

    refactor: retrowave's noice

--- lua/fluoromachine/highlights/retrowave/noice.lua
+++ lua/fluoromachine/highlights/retrowave/noice.lua
@@ -4,12 +4,13 @@ local M = {}
 M.load = function(opts)
   local hl = opts.utils.set_hl
   local c = opts.colors
+  local config = opts.config
 
   hl('NoiceFormatProgressTodo', { fg = c.cyan, bg = c.bgdark })
   hl('NoiceFormatProgressDone', { fg = c.cyan, reverse = true })
   hl('NoiceLspProgressSpinner', { fg = c.red })
   hl('NoiceLspProgressClient', { fg = c.red })
-  hl('NoiceLspProgressTitle', { link = 'Title' })
+  hl('NoiceLspProgressTitle', { fg = c.yellow, bold = config.glow }, { glow = config.glow })
 end
 
 return M
