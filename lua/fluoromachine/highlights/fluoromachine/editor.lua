3a5b44700892a8680c19c2c449b0b6cc3dd81ed7
Max Miliano <milianordelcanto@gmail.com>
2024-06-11 14:30:41 -0300

    refactor: editor,syntax

--- lua/fluoromachine/highlights/fluoromachine/editor.lua
+++ lua/fluoromachine/highlights/fluoromachine/editor.lua
@@ -8,10 +8,11 @@ M.load = function(opts)
   local darken = opts.color.darken
   local lighten = opts.color.lighten
 
+  -- h: highlights-groups
   if config.transparent then
-    hl('ColorColumn', { bg = c.bg }) -- used for columns
+    hl('ColorColumn', { bg = c.bg })
   else
-    hl('ColorColumn', { bg = c.bgdark }) -- used for columns
+    hl('ColorColumn', { bg = c.bgdark })
   end
 
   if config.glow then
@@ -19,76 +20,76 @@ M.load = function(opts)
     hl('CursorLineNr', { fg = c.purple, bg = c.bg, bold = true }, { transparent = config.transparent })
   else
     hl('LineNr', { fg = c.comment, bg = c.bg }, { transparent = config.transparent })
-    hl('CursorLineNr', { fg = c.pink, bg = c.bg }, { transparent = config.transparent }) -- Like LineNr when 'cursorline' is set
+    hl('CursorLineNr', { fg = c.pink, bg = c.bg }, { transparent = config.transparent })
   end
 
-  hl('Conceal', { fg = c.pink }) -- placeholder characters
-  hl('CurSearch', { fg = lighten(c.fg, 100), bg = c.editor.selection }) -- highlight under cursor
-  hl('Cursor', { fg = c.editor.cursor_fg, bg = c.editor.cursor_bg }) -- character under cursor
-  hl('lCursor', { link = 'Cursor' }) -- character under the cursor
-  hl('CursorIM', { link = 'Cursor' }) -- like cursor, but IME mode
-  hl('CursorColumn', { link = 'ColorColumn' }) -- screen-column at the cursor
-  hl('CursorLine', { bg = c.editor.currentline }) -- screen-line at the cursor
-  hl('Directory', { fg = c.yellow }) -- directory names
-  hl('DiffAdd', { fg = c.git.add }) -- Added line
-  hl('DiffChange', { fg = c.git.change }) -- Changed line
-  hl('DiffDelete', { fg = c.bg, bg = c.git.delete }) -- Deleted line
-  hl('DiffText', { fg = c.cyan, reverse = true }) -- Changed Text
-  hl('EndOfBuffer', { fg = c.bg }) -- Filler lines (~)
-  hl('TermCursor', { link = 'Cursor' }) -- Cursor in a focused terminal
-  hl('TermCursorNC', { link = 'Cursor' }) -- Cursor in an unfocused terminal
-  hl('ErrorMsg', { fg = c.diag.error }) -- Error messages on the command line
-  hl('WinSeparator', { fg = c.editor.separator, bg = c.bgdark }, { transparent = config.transparent }) -- Separators between window splits
-  hl('Folded', { fg = c.comment, bg = c.editor.selection }) -- Line used for closed folds
-  hl('FoldColumn', { fg = c.fg, bg = c.bg }) -- 'foldcolumn'
-  hl('SignColumn', { bg = c.bg }, { transparent = config.transparent }) -- Column were signs are displayed
-  hl('IncSearch', { fg = c.cyan }, { transparent = config.transparent }) -- 'incsearch' highlighting, also for the text replaced
-  hl('Substitute', { link = 'IncSearch' }) -- :substitute replacement text highlight
+  hl('Conceal', { fg = c.pink })
+  hl('CurSearch', { fg = lighten(c.fg, 100), bg = c.editor.selection })
+  hl('Cursor', { fg = c.editor.cursor_fg, bg = c.editor.cursor_bg })
+  hl('lCursor', { link = 'Cursor' })
+  hl('CursorIM', { link = 'Cursor' })
+  hl('CursorColumn', { link = 'ColorColumn' })
+  hl('CursorLine', { bg = c.editor.currentline })
+  hl('Directory', { fg = c.yellow })
+  hl('DiffAdd', { fg = c.git.add })
+  hl('DiffChange', { fg = c.git.change })
+  hl('DiffDelete', { fg = c.bg, bg = c.git.delete })
+  hl('DiffText', { fg = c.cyan, reverse = true })
+  hl('EndOfBuffer', { fg = c.bg })
+  hl('TermCursor', { link = 'Cursor' })
+  hl('TermCursorNC', { link = 'Cursor' })
+  hl('ErrorMsg', { fg = c.diag.error })
+  hl('WinSeparator', { fg = c.editor.separator, bg = c.bgdark }, { transparent = config.transparent })
+  hl('Folded', { fg = c.comment, bg = c.editor.selection })
+  hl('FoldColumn', { fg = c.fg, bg = c.bg })
+  hl('SignColumn', { bg = c.bg }, { transparent = config.transparent })
+  hl('IncSearch', { fg = c.cyan }, { transparent = config.transparent })
+  hl('Substitute', { link = 'IncSearch' })
   -- Line number for ":number" and ":#" commands
-  hl('LineNrAbove', { link = 'LineNr' }) -- Line number, above the cursor line
-  hl('LineNrBelow', { link = 'LineNr' }) -- Line number, below the cursor
-  hl('CursorLineFold', { link = 'FoldColumn' }) -- Like FoldColumn when 'cursorline' is set
-  hl('CursorLineSign', { link = 'SignColumn' }) -- Like SignColumn when 'cursorline' is set
-  hl('MatchParen', { fg = c.diag.hint }) -- Character under the cursor or just before it
-  hl('ModeMsg', { link = 'Normal' }) -- 'showmode' message (e.g., "-- INSERT --")
-  hl('MsgArea', { link = 'Normal' }) -- 'Area for messages and cmdline'
-  hl('MsgSeparator', { link = 'Normal' }) -- Separator for scrolled messages msgsep.
-  hl('MoreMsg', { fg = c.cyan, bold = true }) -- more-prompt
-  hl('NonText', { fg = c.comment }) -- '@' at the end of the window
-  hl('Normal', { fg = c.fg, bg = c.bg }, { transparent = config.transparent }) -- Normal text
-  hl('NormalFloat', { fg = c.fg, bg = c.bgdark }) -- Normal text in floating windows
-  hl('FloatBorder', { link = 'WinSeparator' }) -- Border of floating windows.
-  hl('FloatTitle', { link = 'Title' }) -- Title of float windows.
-  hl('NormalNC', { link = 'Normal' }) -- Normal text in non-current windows.
-  hl('Pmenu', { link = 'NormalFloat' }) -- Popup menu: Normal item
-  hl('PmenuSel', { bg = c.editor.selection }) -- Popup menu: Selected item
-  hl('PmenuKind', { link = 'Pmenu' }) -- Popup menu: Normal item kind
-  hl('PmenuKindSel', { link = 'PmenuSel' }) -- Popup menu: Selected item kind
-  hl('PmenuExtra', { link = 'Pmenu' }) -- Popup menu: Normal item extra text
-  hl('PmenuExtraSel', { link = 'PmenuSel' }) -- Popup menu: Selected item extra text
-  hl('PmenuSbar', { link = 'NormalFloat' }) -- Popup menu: Scrollbar
-  hl('PmenuThumb', { bg = c.pink }) -- Popup menu: Thumb of the scrollbar
-  hl('Question', { fg = c.cyan, bold = true }) -- hit-enter prompt and yes/no questions.
-  hl('QuickFixLine', { bg = c.editor.selection }) -- Current quickfix item in the quickfix window
-  hl('Search', { link = 'Visual' }) -- Last search pattern highlighting
-  hl('SpecialKey', { fg = c.yellow, bold = true }) -- Unprintable characters: Text displayed differently from what it really is.
-  hl('SpellBad', { fg = c.diag.error, sp = c.diag.error, underline = true }) -- Word that is not recognized by the spellchecker.
-  hl('SpellCap', { fg = c.diag.hint, sp = c.diag.hint, underline = true }) -- Word that should start with a capital
-  hl('SpellLocal', { fg = c.diag.info, sp = c.diag.info, underline = true }) -- Word that is recognized by the spellchecker as one that is used in another region
-  hl('SpellRare', { fg = c.diag.warning, sp = c.diag.warning, underline = true }) -- Word that is recognized by the spellchecker as one that is hardly ever used.
-  hl('StatusLine', { fg = c.fg, bg = c.bgdark }) -- Status line of current window.
-  hl('StatusLineNC', { fg = c.bgdark, bg = c.bgdark }) -- Status lines of not-current windows.
-  hl('TabLine', { bg = c.bgdark }) -- Tab pages line, not active tab page label.
-  hl('TabLineFill', { fg = c.fg, bg = c.bgdark }) -- Tab pages line, where there are no labels.
-  hl('TabLineSel', { fg = c.yellow, bg = c.base03 }) -- Tab pages line, active tab page label.
-  hl('Title', { fg = c.yellow, bold = true }) -- Titles for output from ":set all", ":autocmd" etc.
-  hl('Visual', { bg = c.editor.selection }) -- Visual mode selection.
-  hl('VisualNOS', { link = 'Visual' }) -- Visual mode selection when vim is "Not Owning the Selection".
-  hl('WarningMsg', { fg = c.diag.warning }) -- Warning messages.
-  hl('Whitespace', { fg = c.comment }) -- "nbsp", "space", "tab", "multispace", "lead" and "trail" in 'listchars'.
-  hl('WildMenu', { bg = c.base02 }) -- Current match in 'wildmenu' completion.
-  hl('WinBar', { link = 'Pmenu' }) -- Window bar of current window.
-  hl('WinBarNC', { link = 'WinBar' }) -- Window bar of not-current windows.
+  hl('LineNrAbove', { link = 'LineNr' })
+  hl('LineNrBelow', { link = 'LineNr' })
+  hl('CursorLineFold', { link = 'FoldColumn' })
+  hl('CursorLineSign', { link = 'SignColumn' })
+  hl('MatchParen', { fg = c.diag.hint })
+  hl('ModeMsg', { link = 'Normal' })
+  hl('MsgArea', { link = 'Normal' })
+  hl('MsgSeparator', { link = 'Normal' })
+  hl('MoreMsg', { fg = c.cyan, bold = true })
+  hl('NonText', { fg = c.comment })
+  hl('Normal', { fg = c.fg, bg = c.bg }, { transparent = config.transparent })
+  hl('NormalFloat', { fg = c.fg, bg = c.bgdark })
+  hl('FloatBorder', { link = 'WinSeparator' })
+  hl('FloatTitle', { link = 'Title' })
+  hl('NormalNC', { link = 'Normal' })
+  hl('Pmenu', { link = 'NormalFloat' })
+  hl('PmenuSel', { bg = c.editor.selection })
+  hl('PmenuKind', { link = 'Pmenu' })
+  hl('PmenuKindSel', { link = 'PmenuSel' })
+  hl('PmenuExtra', { link = 'Pmenu' })
+  hl('PmenuExtraSel', { link = 'PmenuSel' })
+  hl('PmenuSbar', { link = 'NormalFloat' })
+  hl('PmenuThumb', { bg = c.pink })
+  hl('Question', { fg = c.cyan, bold = true })
+  hl('QuickFixLine', { bg = c.editor.selection })
+  hl('Search', { link = 'Visual' })
+  hl('SpecialKey', { fg = c.yellow, bold = true })
+  hl('SpellBad', { fg = c.diag.error, sp = c.diag.error, underline = true })
+  hl('SpellCap', { fg = c.diag.hint, sp = c.diag.hint, underline = true })
+  hl('SpellLocal', { fg = c.diag.info, sp = c.diag.info, underline = true })
+  hl('SpellRare', { fg = c.diag.warning, sp = c.diag.warning, underline = true })
+  hl('StatusLine', { fg = c.fg, bg = c.bgdark })
+  hl('StatusLineNC', { fg = c.bgdark, bg = c.bgdark })
+  hl('TabLine', { bg = c.bgdark })
+  hl('TabLineFill', { fg = c.fg, bg = c.bgdark })
+  hl('TabLineSel', { fg = c.yellow, bg = c.base03 })
+  hl('Title', { fg = c.yellow, bold = true })
+  hl('Visual', { bg = c.editor.selection })
+  hl('VisualNOS', { link = 'Visual' })
+  hl('WarningMsg', { fg = c.diag.warning })
+  hl('Whitespace', { fg = c.comment })
+  hl('WildMenu', { bg = c.base02 })
+  hl('WinBar', { link = 'Pmenu' })
+  hl('WinBarNC', { link = 'WinBar' })
 end
 
 return M
