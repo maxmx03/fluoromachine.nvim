c0057ff72dd9c51208d8ca191f1c0ce543a95aa3
Max Miliano <milianordelcanto@gmail.com>
2024-06-13 16:38:03 -0300

    fix: delta's lualine

--- lua/lualine/themes/delta.lua
+++ lua/lualine/themes/delta.lua
@@ -6,13 +6,13 @@ return {
   normal = {
     a = { bg = c.pink, fg = darken(c.pink, 50), gui = 'bold' },
     b = { bg = c.fg, fg = c.bg },
-    c = { bg = c.alt_bg, fg = c.fg },
+    c = { bg = c.bgdark, fg = c.fg },
   },
   insert = {
     a = { bg = c.green, fg = darken(c.green, 50), gui = 'bold' },
   },
   visual = {
-    a = { bg = c.purple, fg = darken(c.selection, 50), gui = 'bold' },
+    a = { bg = c.purple, fg = darken(c.editor.selection, 50), gui = 'bold' },
   },
   replace = {
     a = { bg = c.red, fg = darken(c.red, 50), gui = 'bold' },
