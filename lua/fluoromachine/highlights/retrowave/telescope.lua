622573b5490c14d8027f7baf0fbc817c13228033
Max Miliano <milianordelcanto@gmail.com>
2024-06-15 12:07:36 -0300

    refactor: border

--- lua/fluoromachine/highlights/retrowave/telescope.lua
+++ lua/fluoromachine/highlights/retrowave/telescope.lua
@@ -22,7 +22,7 @@ M.load = function(opts)
   -- Border highlight groups.
   --   Use TelescopeBorder to override the default.
   --   Otherwise set them specifically
-  hl('TelescopeBorder', { fg = c.editor.separator, bg = c.bgdark }, { transparent = config.transparent })
+  hl('TelescopeBorder', { link = 'WinSeparator' })
   hl('TelescopePromptBorder', { link = 'TelescopeBorder' })
   hl('TelescopeResultsBorder', { link = 'TelescopeBorder' })
   hl('TelescopePreviewBorder', { link = 'TelescopeBorder' })
